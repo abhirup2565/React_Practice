0d0d160d441d142e973dabbe75c3071b47f337bb
abhirup2565 <abhirup2565@gmail.com>
2025-09-08 15:40:19 +0530

    basic ui for password generation

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const checkboxData=[
+    {
+      title:"Include Uppercase Letters",
+      state:false
+    },
+    {
+      title:"Include Lowercase Letters",
+      state:false
+    },
+    {
+      title:"Include Numbers",
+      state:false
+    },
+    {
+      title:"Include Symbol",
+      state:false
+    }
+  ]
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='title'>afesf124</div>
+        <button className='copyBtn' onClick={()=>{}}>copy</button>
+      </div>
+      <div className='charLength'>
+        <span>
+          <label>Character Length</label>
+          <label>4</label>
+          <input type="range" min='4' max='20'onChange={()=>{}} />
+        </span>
+      </div>
+      <div className='checkboxes'>
+        {checkboxData.map((data,i)=>{
+          return <div key={i}>
+            <input type="checkbox" checked={data.state}/>
+            <label>{data.title}</label>
+            </div>
+        })}
+      </div>
+
+      {/* strength */}
+
+      <button className='generateBtn' onClick={()=>{}}>Generate</button>
+
+
+    </div>
+  )
+}
+
+export default App
